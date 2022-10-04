99a30afd5b98f54a5f8137db69b9131702502ebf
Umoren Gilbert <umorengilbert17@gmail.com>
2022-10-05 00:59:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+ /*The load event is fired when the whole page has loaded, just to say that the codes will run when the page has fully loaded*/
+ window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+
+
+    /* When the form is submitted it doesnt take you to another page or refreshes the page*/
+         form.addEventListener('submit', (e) => {
+              e.preventDefault(); // This stops it from refreshing the page
+             
+
+          const task = input.value; // .value Returns the value stored in the variable
+               if(!task) {
+                   alert('Please fill out the task');
+                   return;
+               } 
+
+         const task_el = document.createElement("div");
+            task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+    
+
+         task_el.appendChild(task_content_el);
+
+         const task_input_el = document.createElement("input");
+         task_input_el.classList.add("text");
+         task_input_el.type ="text";
+         task_input_el.value= task;
+         task_input_el.setAttribute ("readonly", "readonly");
+
+         task_content_el.appendChild(task_input_el);
+
+         const task_actions_el = document.createElement("div");
+         task_actions_el.classList.add("actions");
+
+         const task_edit_el = document.createElement("button");
+         task_edit_el.classList.add("edit");
+         task_edit_el.innerHTML = "Edit";
+
+         const task_delete_el = document.createElement("button");
+         task_delete_el.classList.add("delete");
+         task_delete_el.innerHTML = "Delete";
+
+         task_actions_el.appendChild(task_edit_el);
+         task_actions_el.appendChild(task_delete_el);
+
+         task_el.appendChild(task_actions_el);
+
+
+        list_el.appendChild(task_el);
+
+        input.value ="";
+
+           // The working of the Edit/ Save button 
+        task_edit_el.addEventListener('click', () => {
+            if(task_edit_el.innerText.toLowerCase () == "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText ="Edit";
+            }
+        });
+        // The working of the delete button
+
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
+
+   });
+ });
